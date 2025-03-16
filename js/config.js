3b2201c56110f3ccb18734c492d58b7c03653025
Lalita Vuppuluri <vuppuluri31@gmail.com>
2025-03-16 18:06:40 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_oP30u77vi', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '35tjfng0lotm5mt6ol5gv0m5c1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_G1QRyP6wd', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7mfvjmuo8scfpi9qvftp90e5hf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
