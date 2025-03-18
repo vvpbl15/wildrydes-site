dee0a153c38f1a5ab57a1525bc9306b0a87b83a0
Lalita Vuppuluri <vuppuluri31@gmail.com>
2025-03-18 14:27:53 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_G1QRyP6wd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7mfvjmuo8scfpi9qvftp90e5hf', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_HDk1fVP5t', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3uvpk8qmp6ide2sg0ng62qqno', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
